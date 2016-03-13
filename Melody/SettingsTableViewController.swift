973ab93a559681beb10d19ae44d85b5e9fcfc04a
Raj Abishek <rajabishek@hotmail.com>
2016-03-14 00:09:19 +0530

    Adds the settings table view controller

---
+++ Melody/SettingsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsTableViewController.swift
+//  Melody
+//
+//  Created by Raj Abishek on 14/03/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
