50157342830433aed8a01b25cd420f129395fbf2
Raj Abishek <rajabishek@hotmail.com>
2016-03-14 00:22:21 +0530

    Save the switch states in settings page using NSUserDefaults API

--- Melody/SettingsTableViewController.swift
+++ Melody/SettingsTableViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
 
+    @IBOutlet weak var aboutTextLabel: UILabel!
+    
+    @IBOutlet weak var feedbackTextLabel: UILabel!
+    
+    @IBOutlet weak var securityTextLabel: UILabel!
+
+    @IBOutlet weak var touchIdSwitch: UISwitch!
+    
+    @IBOutlet weak var imageQualityTextLabel: UILabel!
+    
+    @IBOutlet weak var imageQualitySwitch: UISwitch!
+    
+    @IBOutlet weak var apiCountTextLabel: UILabel!
+    
+    @IBOutlet weak var apiCountSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        touchIdSwitch.on = defaults.boolForKey("shouldUseTouchId")
+        imageQualitySwitch.on = defaults.boolForKey("betterImageQuality")
+    }
+    
+    @IBAction func changeImageQualitySettings(sender: UISwitch) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool(imageQualitySwitch.on, forKey: "betterImageQuality")
+    }
+    
+    @IBAction func changeSecuritySettings(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool(touchIdSwitch.on, forKey: "shouldUseTouchId")
     }
 
     override func didReceiveMemoryWarning() {
